bf9e23c4dfaee832645b8d8baf4a571829d95589
KebPericles <45951033+KebPericles@users.noreply.github.com>
2025-03-23 02:55:01 -0600

    feat(ListaMaterias): Bosquejo inicial

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import SelectorMaterias from "./components/SelectorMaterias.svelte";
+	import ListaMaterias from "./components/ListaMaterias.svelte";
+import SelectorMaterias from "./components/SelectorMaterias.svelte";
 	import VisualizadorHorario from "./components/VisualizadorHorario.svelte";
 	import {
 		crearCombinaciones,
@@ -31,25 +32,12 @@
 <header class="justify-between w-full py-4">
 	<h1>Generador de horarios UPIITA</h1>
 </header>
-<div class="flex flex-row gap-4 p-4 max-h-[60svh]">
+<div class="flex flex-row gap-4 p-4 h-[65svh]">
 	<SelectorMaterias
 		{todasLasMaterias}
 		bind:materiasSeleccionadas
-		classs=" w-[40%] md:w-[35%] flex flex-col"
 	/>
-	<article class=" w-[60%] md:w-[65%] flex overflow-y-auto">
-		<h2>Este es el generador de horario UPIITA</h2>
-
-		<!-- 
-  Precondiciones:
-  - Datos de materias ya cargados en el servidor
-
-  Proceso:
-  - Escoger materia
-  -->
-
-		<pre>{JSON.stringify(materiasSeleccionadas)}</pre>
-	</article>
+	<ListaMaterias {materiasSeleccionadas} />
 </div>
 <VisualizadorHorario {horariosPosibles} {cantidadDeMateriasSeleccionadas} />
 
---
+++ src/components/ListaMaterias.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Materia } from "../lib/Materias";
+
+	interface Props {
+		materiasSeleccionadas: Materia[];
+	}
+
+	let { materiasSeleccionadas = $bindable([]) }: Props = $props();
+</script>
+
+<article class="w-full md:w-full flex overflow-y-auto">
+	{#if materiasSeleccionadas.length > 0}
+		<ul class="flex flex-col w-full">
+			{#each materiasSeleccionadas as materia}
+				<li class="bg-amber-800 rounded-xl p-2 m-0.5">
+					<div class="flex flex-row gap-1 items-center">
+						<div class="min-w-16 h-full rounded-xl p-2">
+							{materia.grupo}
+						</div>
+						<div class="w-full h-full bg-amber-500 rounded-xl p-2">
+							{materia.nombre}
+						</div>
+						<div class="w-full h-full bg-amber-500 rounded-xl p-2">
+							{materia.profesor}
+						</div>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<h3 class="text-center place-self-center text-2xl">
+			Empieza a seleccionar materias para generar un horario. Aquí aparecerán
+			todas las materias seleccionadas.
+		</h3>
+	{/if}
+</article>
