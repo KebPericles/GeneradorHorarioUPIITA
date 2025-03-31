fc1a7310d07db5486ab049dbe3c816ac1c61c2fb
KebPericles <45951033+KebPericles@users.noreply.github.com>
2025-03-30 22:09:46 -0600

    feat(ListaMaterias): UX autoscroll, UI scrollbar

--- src/components/ListaMaterias.svelte
+++ src/components/ListaMaterias.svelte
@@ -6,11 +6,28 @@
 	}
 
 	let { materiasSeleccionadas = $bindable([]) }: Props = $props();
+	let materiasSeleccionadasAnterior: Materia[] = [];
+
+	$effect(() => {
+		if (materiasSeleccionadas.length > materiasSeleccionadasAnterior.length) {
+			document
+				.getElementById("lista-materias")
+				?.lastElementChild?.scrollIntoView({
+					behavior: "smooth",
+					block: "end",
+					inline: "end",
+				});
+		}
+
+		materiasSeleccionadasAnterior = $state.snapshot(
+			materiasSeleccionadas
+		) as Materia[];
+	});
 </script>
 
 <article class="w-full md:w-full flex overflow-y-auto">
 	{#if materiasSeleccionadas.length > 0}
-		<ul class="flex flex-col w-full">
+		<ul class="flex flex-col w-full" id="lista-materias">
 			{#each materiasSeleccionadas as materia}
 				<li class="bg-amber-800 rounded-xl p-2 m-0.5">
 					<div class="flex flex-row gap-1 items-center">
@@ -34,3 +51,30 @@
 		</h3>
 	{/if}
 </article>
+
+<style>
+	/* width */
+	::-webkit-scrollbar {
+		width: 8px;
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: #f1f1f100;
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #888;
+	}
+
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #555;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+</style>
